ada0867e537657dd20c2b1bedfbf29c3e2d4f401
alibaba0010 <zakariyahali100@gmail.com>
2024-09-09 20:11:12 +0100

    handling register func

--- frontend/scripts/register.js
+++ frontend/scripts/register.js
@@ -3,20 +3,39 @@ document
   .addEventListener("submit", async function (event) {
     event.preventDefault();
     const username = document.getElementById("username").value;
-    const contact_number = document.getElementById("contact_number").value;
-    const email = document.getElementById("email").value;
+    const checkEmail = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    const response = await fetch("http://127.0.0.1:4001/auth/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password, email, contact_number }),
-      credentials: "include",
-    });
-    const result = await response.json();
-    // handle response
-    if (result.ok) {
-      window.location.href = "./index.html";
+    const confirmPassword = document.getElementById("confirm-password").value;
+    const email = isValidEmail(checkEmail);
+    if (!email) {
+      alert("Invalid email");
+    } else if (password !== confirmPassword) {
+      alert("Password does not match");
     } else {
-      alert(result.message);
+      const response = await fetch(
+        "http://127.0.0.1:5000/api/v1/auth/register",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            username,
+            email,
+            password,
+            confirmPassword,
+          }),
+          credentials: "include",
+        }
+      );
+      const result = await response.json();
+      if (result.ok) {
+        window.location.href = "../components/login.html";
+      } else {
+        alert(result.message);
+      }
     }
   });
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+}
