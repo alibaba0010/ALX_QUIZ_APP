94f8da4a660981b39cd03c91197e22a333050d79
alibaba0010 <zakariyahali100@gmail.com>
2024-09-14 17:27:02 +0100

    Adding functionalities to quiz

--- frontend/scripts/quiz.js
+++ frontend/scripts/quiz.js
@@ -15,7 +15,7 @@ const loadQuiz = async () => {
     const { userId, data } = result;
     totalScore.innerHTML = data.length;
     //  showTime(datalength);
-    if (!checkQuizResultExists(userId)) {
+    if (!checkQuizQuestionsExists(userId)) {
       // If not, store the new quiz data
       storeQuizQuestion(userId, data);
     }
@@ -74,71 +74,127 @@ async function loadQuestion(data) {
     questionElement[i].innerHTML = questionNo + ". " + currentQuestion.question;
   }
   answersElement.innerHTML = "";
-  const answerButton = document.createElement("button");
   for (const key in answers) {
+    const answerButton = document.createElement("button");
     if (answers.hasOwnProperty(key) && answers[key] !== null) {
       const value = answers[key];
       answerButton.innerHTML = value;
+      answerButton.dataset.questionKey = key;
       answerButton.classList.add("answer-btn");
       answersElement.appendChild(answerButton);
       if (value == null) {
         answersElement.display = "none";
       }
     }
-  }
-  console.log("Current Question: ", currentQuestion);
-  const { correct_answers } = currentQuestion;
-  const correctAnswers = [];
-  for (const [key, value] of Object.entries(correct_answers)) {
-    if (value === "true") {
-      correctAnswers.push(key);
-      answerButton.dataset.correctAnswer = key;
+    const { correct_answers } = currentQuestion;
+    const correctAnswers = [];
+    for (const [key, value] of Object.entries(correct_answers)) {
+      if (value === "false") {
+        // change to true
+        correctAnswers.push(key);
+      }
     }
+    answerButton.dataset.correctAnswers = correctAnswers;
+    answerButton.addEventListener("click", selectAnswer);
   }
-  console.log("correctAnswers", correctAnswers);
-  // if (option.correctAnswer) {
-  //   answerButton.dataset.correctAnswer = option.correctAnswer;
-  //   // console.log(answerButton.dataset.correctAnswer);
-  // }
-  answerButton.addEventListener("click", selectAnswer);
 }
 
+let currentScore = 0;
 function selectAnswer(event) {
-  const selectedAnswer = event.target;
-  console.log("selectedAnswer", selectedAnswer);
-  // const correctAnswer = selectedAnswer.dataset.correctAnswer === "true"; // needed to add "true" cos if not true it returns undefined
-  // // console.log("Correct Answer: ", correctAnswer);
-  // if (correctAnswer) {
-  //   score++;
-  //   selectedAnswer.classList.add("correctAnswer");
-  //   // console.log("Correct Answer: ", correctAnswer);
-  //   // console.log("Score: ", score);
-  // } else {
-  //   selectedAnswer.classList.add("wrongAnswer");
-  //   // console.log("Wrong Answer: ", correctAnswer);
-  //   // console.log("Score: ", score);
-  // }
-  // Array.from(answersElement.children).forEach((answer) => {
-  //   if (answersElement.dataset.correctAnswer === "true") {
-  //     answer.classList.add("correctAnswer");
-  //   }
-  //   answer.disabled = true;
-  // });
-  // submitButton.style.display = "block";
-  // submitButton.style.display = "inline";
-  // selectedAnswer.classList.add("correct");
-  // setTimeout(() => {
-  //   selectedAnswer.classList.remove("correct");
-  // }, 1000);
-  // console.log("Current Question Index: ", currentQuestionIndex);
-  // console.log("Quiz Data Length: ", quizData.length);
-  if (currentQuestionIndex < quizData.length) {
-    submitButton.innerHTML = "Next";
+  const selectedButton = event.target;
+  const selectedKey = selectedButton.dataset.questionKey;
+  console.log("SelectAnswer: " + selectedKey);
+  const correctAnswers = selectedButton.dataset.correctAnswers.split(",");
+  console.log("CorrectAnswer: " + correctAnswers);
+  if (correctAnswers.includes(`${selectedKey}_correct`)) {
+    // Calculate score increment
+    const scoreIncrement = 1 / correctAnswers.length;
+
+    // Increment the score
+    currentScore += scoreIncrement;
+
+    // Update the score display
+    // updateScoreDisplay();
+
+    console.log(
+      `Correct! Button ${selectedKey.toUpperCase()} is a right answer.`
+    );
+    console.log(
+      `Score increased by ${scoreIncrement.toFixed(
+        2
+      )}. New total: ${currentScore.toFixed(2)}`
+    );
+
+    // Add visual feedback for correct answer
+    selectedButton.classList.add("correct-answer");
   } else {
-    submitButton.innerHTML = "Finish";
-    // Display score
+    console.log(`Sorry, button ${selectedKey.toUpperCase()} is not correct.`);
+    // Add visual feedback for incorrect answer
+    selectedButton.classList.add("incorrect-answer");
   }
 }
+
+// function selectAnswer(event) {
+//   const selectedAnswer = event.target;
+//   const correctAnswers = selectedAnswer.dataset.correctAnswers.split(",");
+//   const { questionKey } = selectedAnswer.dataset;
+//   const correctAnswer = correctAnswers.includes(`${questionKey}_correct`);
+//   // const correctAnswer = correctAnswers.includes(questionKey);
+//   let currentScore = 0;
+//   if (correctAnswer) {
+//     const scoreIncrement = 1 / correctAnswers.length;
+//     console.log("Score increment: " + scoreIncrement);
+//     currentScore += scoreIncrement;
+//     console.log("Total Score: " + currentScore);
+//   }
+//   console.log("Total Score out: " + currentScore);
+
+//   // // Function to disable all answer buttons
+//   // function disableAllButtons() {
+//   //   const buttons = document.querySelectorAll("#answers-container button");
+//   //   buttons.forEach((button) => {
+//   //     button.disabled = true;
+//   //   });
+//   // }
+
+//   // // Add event listeners to answer buttons
+//   // document.querySelectorAll('#answers-container button').forEach(button => {
+//   //   button.addEventListener('click', selectAnswer);
+//   // });
+
+//   // const correctAnswer = selectedAnswer.dataset.correctAnswer === "true"; // needed to add "true" cos if not true it returns undefined
+//   // // console.log("Correct Answer: ", correctAnswer);
+//   // if (correctAnswer) {
+//   //   score++;
+//   //   selectedAnswer.classList.add("correctAnswer");
+//   //   // console.log("Correct Answer: ", correctAnswer);
+//   //   // console.log("Score: ", score);
+//   // } else {
+//   //   selectedAnswer.classList.add("wrongAnswer");
+//   //   // console.log("Wrong Answer: ", correctAnswer);
+//   //   // console.log("Score: ", score);
+//   // }
+//   // Array.from(answersElement.children).forEach((answer) => {
+//   //   if (answersElement.dataset.correctAnswer === "true") {
+//   //     answer.classList.add("correctAnswer");
+//   //   }
+//   //   answer.disabled = true;
+//   // });
+//   // submitButton.style.display = "block";
+//   // submitButton.style.display = "inline";
+//   // selectedAnswer.classList.add("correct");
+//   // setTimeout(() => {
+//   //   selectedAnswer.classList.remove("correct");
+//   // }, 1000);
+//   // console.log("Current Question Index: ", currentQuestionIndex);
+//   // console.log("Quiz Data Length: ", quizData.length);
+//   // if (currentQuestionIndex < quizData.length) {
+//   //   submitButton.innerHTML = "Next";
+//   // } else {
+//   //   submitButton.innerHTML = "Finish";
+//   //   // Display score
+//   // }
+// }
 function checkMultipleChoice(data) {
   const currentQuestion = data[currentQuestionIndex];
   const currentChoice = currentQuestion.multiple_correct_answers;
@@ -188,7 +244,7 @@ function getQuizQuestion(userId) {
     return null;
   }
 }
-function checkQuizResultExists(quizId) {
+function checkQuizQuestionsExists(quizId) {
   try {
     if (typeof Storage !== "undefined") {
       const result = localStorage.getItem(`quiz_${quizId}`);
