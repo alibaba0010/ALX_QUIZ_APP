b06def44df9a9130284c4ebf575ff0c36e5c29ab
alibaba0010 <zakariyahali100@gmail.com>
2024-09-16 20:08:38 +0100

    Adding question schema

--- server/models/questionDB.js
+++ server/models/questionDB.js
@@ -2,7 +2,15 @@ import pkg, { Types } from "mongoose";
 const { Schema, model } = pkg;
 
 const QuestionSchema = new Schema({
-   user: { type: Types.ObjectId
+  user: { type: Types.ObjectId, ref: "User", required: true },
+  data: [
+    {
+      questions: { type: String },
+      answers: { type: [String] },
+      correct_answer: { type: [String] },
+      multiple_choice: { type: Boolean },
+    },
+  ],
 });
 
 export default model("Question", QuestionSchema);
