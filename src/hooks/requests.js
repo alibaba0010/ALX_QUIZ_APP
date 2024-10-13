b8b962f34a324f0e9165dbe5e8b6c73b7e406cd9
alibaba0010 <zakariyahali100@gmail.com>
2024-10-13 18:13:11 +0100

    error solved in route

--- src/hooks/requests.js
+++ src/hooks/requests.js
@@ -1,33 +1,23 @@
-const API_URL = "http://127.0.0.1:5000/api/v1/";
+const API_URL = "http://127.0.0.1:5000/api/v1";
 export const registerUser = async (
   username,
   email,
   password,
   confirmPassword
 ) => {
-  try {
-    const response = await fetch(`${API_URL}/user/register`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        username,
-        email,
-        password,
-        confirmPassword,
-      }),
-      credentials: "include",
-    });
+  const response = await fetch(`${API_URL}/user/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username,
+      email,
+      password,
+      confirmPassword,
+    }),
+    credentials: "include",
+  });
 
-    const result = await response.json();
+  const result = await response.json();
 
-    //   if (result.data) {
-    //       window.location.href = "/login";
-    //   } else {
-    //     alert(result.msg);
-    //   }
-    return result.data;
-  } catch (error) {
-    console.error("Error:", error);
-    alert("An error occurred. Please try again.");
-  }
+  return result;
 };
